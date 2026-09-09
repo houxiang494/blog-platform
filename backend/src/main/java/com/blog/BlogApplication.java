2177fc8ec598074219dca1e3e0506f27a5c79feb
houxiang494 <houxiang494@gmail.com>
2026-09-09 10:11:16 +0800

    添加Spring Boot启动类

---
+++ backend/src/main/java/com/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Blog Platform 应用入口
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
