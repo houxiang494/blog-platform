3a2cc4ff73d683493de1a997f10548ee9042327e
houxiang494 <houxiang494@gmail.com>
2026-09-09 10:10:20 +0800

    添加UserRepository

---
+++ backend/src/main/java/com/blog/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.blog.repository;
+
+import com.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * 用户数据访问接口
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<User> findByEmail(String email);
+    
+    Optional<User> findByUsername(String username);
+    
+    boolean existsByEmail(String email);
+    
+    boolean existsByUsername(String username);
+    
+}
